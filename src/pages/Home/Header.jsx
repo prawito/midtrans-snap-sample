0882b1b6981e8a09b92c3aa1b5c7f971e3273d36
Ali Qornan <ali.qornan@go-jek.com>
2023-11-13 14:21:23 +0700

    [Ali] Update environment and README

--- src/pages/Home/Header.jsx
+++ src/pages/Home/Header.jsx
@@ -6,7 +6,7 @@ export const Header = () => {
 
     return (
       <div className='header-banner'>
-        <p className='logo-title'>Ndoro Store</p>
+        <p className='logo-title'>Ali Rock Store</p>
         <div className='icon-check-order' onClick={() => navigate('/order-status')}>
           <img src={historyImage} alt='order-status' />
         </div>
