9d82117412618ce98c6e6146e47e32fceecec8f6
Ali Qornan <ali.qornan@go-jek.com>
2023-11-15 09:43:37 +0700

    [Ali] Put cors on function

--- functions/index.js
+++ functions/index.js
@@ -4,24 +4,26 @@ const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const { initializeApp } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
-
 initializeApp();
-
 const db = getFirestore();
 
-exports.charge = onRequest(async (request, response) => {
-  console.log(request.body);
-  const midtransUrl = `${process.env.MIDTRANS_API_URL}/snap/v1/transactions`;
-  const midtransResponse = await fetch(midtransUrl, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': process.env.MIDTRANS_SERVER_AUTHORIZATION,
-    },
-    body: request.body
-  }).then((res) => res.json())
-  console.log(midtransResponse);
-  response.send(midtransResponse);
+exports.charge = onRequest({ cors: [/firebase\.com$/, /web\.app$/, /firebaseapp\.com$/] },
+  async (request, response) => {
+    console.log(request.body);
+    const midtransUrl = `${process.env.MIDTRANS_API_URL}/snap/v1/transactions`;
+    const midtransResponse = await fetch(midtransUrl, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': process.env.MIDTRANS_SERVER_AUTHORIZATION,
+      },
+      body: request.body
+    }).then((res) => {
+      response.status(res.status);
+      return res.json();
+    })
+    
+    response.send(midtransResponse);
 });
 
 
@@ -31,12 +33,17 @@ exports.notify = onRequest(async (request, response) => {
   const transactionId = requestBody.order_id
   const docRef = db.collection('transactions').doc(transactionId);
 
-  await docRef.update({
-    payment_method: requestBody.payment_type,
-    status: requestBody.transaction_status,
-  });
+  var result = ""
+  try { 
+    await docRef.update({
+      payment_method: requestBody.payment_type,
+      status: requestBody.transaction_status,
+    });
+    result = "Success"
+  } catch ({ name, message }) {
+    result = message 
+  }
 
-  const result = "Done"
   console.log(result);
   response.send(result)
 });
